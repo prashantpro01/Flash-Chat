e5f56ac7994bc3924d744dfadba24781bbb60ca5
Ahraar Alam <ahraralam2012@gmail.com>
2020-01-02 02:56:31 +0530

    initial

---
+++ lib/components/Rounded_Button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class RoundedButtton extends StatelessWidget {
+  RoundedButtton({this.title, this.colour, @required this.onPressed});
+
+  final Color colour;
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: this.colour,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: this.onPressed,
+          minWidth: 200.0,
+          textColor: Colors.white,
+          height: 42.0,
+          child: Text(
+            this.title,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,99 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/Rounded_Button.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    animation =
+        ColorTween(begin: Colors.grey, end: Colors.white).animate(controller);
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 400.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ColorizeAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  isRepeatingAnimation: true,
+                  textStyle: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.grey,
+                  ),
+                  colors: [
+                    Colors.grey,
+                    Colors.blue,
+                    Colors.yellow,
+                    Colors.red,
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RoundedButtton(
+              title: 'Log In',
+              colour: Colors.lightBlueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButtton(
+              title: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+            Column(
+              children: <Widget>[
+                Text('by PRASHANT'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
